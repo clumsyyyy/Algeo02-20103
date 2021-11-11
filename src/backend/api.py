0bb17e5ec39eb553828983da70c4a31ef67bde35
marfgold1 <34912365+marfgold1@users.noreply.github.com>
2021-11-12 01:31:24 +0700

    Backend dones?

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,6 +1,7 @@
 import io
 from connexion import request
 from flask import send_file
+from alingeo.imaging.compress import compressSVD
 
 def compress(body):
     """
@@ -8,6 +9,13 @@ def compress(body):
     """
     print(body)
     file = io.BytesIO()
-    request.files.get('imageFile').save(file)
+    fileReq = request.files.get('imageFile')
+    fileReq.save(file)
     file.seek(0)
-    return send_file(file, as_attachment=True, attachment_filename='compressed.jpg')
+    res = compressSVD(file)
+    file.close()
+    file = io.BytesIO()
+    res.save(file, fileReq.mimetype[6:])
+    res.save('test.jpg')
+    file.seek(0)
+    return send_file(file, as_attachment=True, attachment_filename=f'compressed.{fileReq.mimetype[6:]}')
