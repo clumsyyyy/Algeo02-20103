9e733eb9e0540ec7c74730335a122fabe6aa6c9a
clumsyyyy <owencwijaya@gmail.com>
2021-11-14 12:29:22 +0700

    minor touches

--- src/frontend/src/components/Results.js
+++ src/frontend/src/components/Results.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react'
-import "./Results.css"
-import axios from 'axios'
 import { ImgContext } from './ImageProvider'
+import axios from 'axios'
+import "./Results.css"
+
+
 
 export const Results = () =>{
     const imgCtx = useContext(ImgContext);
@@ -10,9 +12,11 @@ export const Results = () =>{
         var scale = parseInt(document.getElementById('scale').value);
         var iteration = parseInt(document.getElementById('iter').value);
         if (scale <= 0 || scale > 100 || isNaN(scale)){
-            alert("Nilai skala harus di antara 1 hingga 100!")
+            alert("Scale value must be between 1 and 100!")
         } else if (iteration <= 0 || iteration > 100 || isNaN(iteration)){
-            alert("Nilai iterasi harus di antara 1 hingga 100!")
+            alert("Iteration value must be between 2 and 100!")
+        } else if (imgCtx.isProcessing === true){
+            alert("Could not compress - another process is running!");
         } else {
             imgCtx.setIsProcessing(true);
             imgCtx.setResultImg(null);
@@ -23,6 +27,7 @@ export const Results = () =>{
             var alphaBool = document.getElementById('alpha-checkbox').value === "on" ? true : false
 
             const data = new FormData()
+            console.log(eigenRatio, " ", scale, " ", alphaBool, " ", iteration);
             data.append('imageFile', imgCtx.image);
             data.append('eigenRatio', eigenRatio);
             data.append('scale', scale);
@@ -64,6 +69,8 @@ export const Results = () =>{
         link.setAttribute('download', imgCtx.image.name);
         link.click();
     }
+
+
     
     return(
         <div id = "Results" className = "Results" hidden>
@@ -72,31 +79,31 @@ export const Results = () =>{
             <div className = "containers">
 
                 <img id = "preview-box" className = "container" alt = "preview"></img>
-
+                
                 <div className = "slider-container">
                     <h3 className = "section-title">Set Eigen Ratio</h3>
                     <p className = "left-p">Low</p>
                     <p className = "right-p">High</p>
-                    <input id = "eigen-slider" type="range" min="1" max="100"></input>
+                    <input id = "eigen-slider" type="range" min="1" max="100" step = "10" defaultValue = "20"></input>
                     <p className = "expository-paragraph"><br/>Higher Eigen Ratio means a sharper compressed image quality albeit more time to process.</p>
                 </div>
 
                 <div className = "scale-input">
                     <h3 className = "section-title"> Set Image Scale </h3>
-                    <input className = "scale" id = "scale" type = "number" placeholder = "Enter image scale... (1-100)" required min = "1" max = "100"></input>
+                    <input className = "scale" id = "scale" type = "number" placeholder = "Enter image scale... (1-100)" required min = "0" max = "100" defaultValue = "100"></input>
                     <p className = "expository-paragraph">Higher image scale will create a higher-resolution compressed image albeit more time to process.</p>
                 </div>
 
                 <div className = "iter-input">
                     <h3 className = "section-title"> Set Iteration</h3>
-                    <input className = "iter" id = "iter" type = "number" placeholder = "Enter iteration number... (1-100)" required min = "1" max = "100"></input>
+                    <input className = "iter" id = "iter" type = "number" placeholder = "Enter iteration number... (2-100)" required min = "0" max = "100" defaultValue = "2"></input>
                     <p className = "expository-paragraph">Higher iteration count would take more time, but may result in better image quality.</p>
                 </div>
 
                 <div className = "alpha-layer">
                     <label htmlFor = "alpha-checkbox">
                         Preserve alpha layer? &nbsp;&nbsp;&nbsp;
-                        <input id = "alpha-checkbox" type = "checkbox"></input>
+                        <input id = "alpha-checkbox" type = "checkbox" defaultChecked></input>
                     </label>
                     <p className = "expository-paragraph">(mark for images with transparent elements)</p>
                 </div>
@@ -125,6 +132,9 @@ export const Results = () =>{
                     2021, 'Newo Social Credit'
                 </div>
             </div>
+            <script>
+                document.getElementById("alpha-checkbox").defaultChecked = "20";
+            </script>
         </div>
     )
 }
